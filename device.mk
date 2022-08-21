40e4fcc9b5caa919b142919294fc6b2d58e69e5c
madmax7896 <madmax78968@gmail.com>
2022-08-21 13:27:40 +0530

    munch: keylayouts and idc stuff squash: - munch: Import keylayouts and idc froms stock - munch: keylayout: Blacklist all FP hal keyevents

--- device.mk
+++ device.mk
@@ -44,6 +44,14 @@ PRODUCT_SHIPPING_API_LEVEL := 30
 PRODUCT_PACKAGES += \
     lib-watermarkshim
 
+# IDC
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/idc/,$(TARGET_COPY_OUT_VENDOR)/usr/idc)
+
+# Keylayout
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/keylayout/,$(TARGET_COPY_OUT_VENDOR)/usr/keylayout)
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
